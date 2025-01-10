c5bd798de1eaa96e31abd97de49a580880eb7808
angga <erlanggaerzon9@gmail.com>
2025-01-10 11:24:26 +0700

    commit dashboard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,7 @@
-// 'use client'
 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-// import Login from "./pages/login";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
