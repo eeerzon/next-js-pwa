6d8431060abf801635641815b68c792ba5e31f2c
angga <erlanggaerzon9@gmail.com>
2025-01-10 09:44:24 +0700

    fix bug login

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-'use client'
+// 'use client'
 
 import Image from "next/image";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
+// import { useEffect } from "react";
+// import { useRouter } from "next/router";
 
 
 export default function Home() {
-  const router = useRouter();
+  // const router = useRouter();
 
-  useEffect(() => {
-    router.replace("./pages/login"); // Mengarahkan ke halaman login
-  }, [router]);
+  // useEffect(() => {
+  //   router.replace("/login"); // Mengarahkan ke halaman login
+  // }, [router]);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
