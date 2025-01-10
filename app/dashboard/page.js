669efbc2cabda2dd2e2d3f758b62250e22ecf30c
angga <erlanggaerzon9@gmail.com>
2025-01-10 18:03:48 +0700

    fixing bug next router

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,10 +1,24 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+
+
 export default function Dashboard() {
+
+    const router = useRouter();
+
+  const navigateToMenu1 = () => {
+    router.push('/customer-input'); // Ganti '/menu1' dengan rute yang sesuai
+  };
+
     return (
       <div className="min-h-screen flex flex-col md:flex-row">
         <aside className="w-64 bg-gray-800 text-white hidden md:block">
           <nav>
             <ul>
-              <li className="p-4">Menu 1</li>
+            <li className="p-4 cursor-pointer hover:bg-gray-700" onClick={navigateToMenu1}>
+              Input Customer Data
+            </li>
               <li className="p-4">Menu 2</li>
             </ul>
           </nav>
