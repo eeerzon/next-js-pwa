6963e81fd6663c7ae02e4ccb0052ec411d2cd9c0
angga <erlanggaerzon9@gmail.com>
2025-01-10 11:03:52 +0700

    fix bug login

--- app/page.tsx
+++ app/page.tsx
@@ -119,8 +119,9 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
-      <Image
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+         <Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
@@ -128,29 +129,35 @@ export default function LoginPage() {
           height={38}
           priority
         />
-      <div className="bg-white dark:bg-gray-800 p-6 rounded shadow-md w-80">
-        <h2 className="text-lg font-bold text-center mb-4">Login</h2>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          className="w-full p-2 mb-4 border rounded text-black"
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="w-full p-2 mb-4 border rounded text-black"
-        />
-        <button
-          onClick={handleLogin}
-          className="w-full bg-blue-500 text-white py-2 rounded"
-        >
-          Login
-        </button>
-      </div>
+
+
+        <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900">
+          <div className="bg-white dark:bg-gray-800 p-6 rounded shadow-md w-80">
+            <h2 className="text-lg font-bold text-center mb-4">Login</h2>
+              <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="w-full p-2 mb-4 border rounded text-black"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full p-2 mb-4 border rounded text-black"
+              />
+              <button
+                onClick={handleLogin}
+                className="w-full bg-blue-500 text-white py-2 rounded">
+                  Login
+                </button>
+              </div>
+          </div>
+
+        </main>
     </div>
+    
   );
 }
